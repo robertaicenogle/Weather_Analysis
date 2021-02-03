c3867299337e9243423863eab62db95b681aa6b0
Robert Icenogle <75597724+robertaicenogle@users.noreply.github.com>
2021-02-02 22:39:56 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-weather_api_key=""
+weather_api_key="bdcd2f4e896bc3ba596f00fee7f607e1"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyD-2CCis1L-eU3ZPnKOYo59ayKqThERJQA"
 
 
 # I deleted the API keys for this project. I can re-add them if needed.
